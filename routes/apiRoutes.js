8ae3b13bd6f02bd4d6a96eec020a5ca7bca8e34c
Cody K <ckietzm2@gmail.com>
2023-10-19 10:24:35 -0500

    Created UUID folder for random ID generator and completed apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,28 @@
 // Post Route
 // Delete Route
 const router = require('express').Router();
+const uuid = require('../helpers/uuid');
 
 router.get('/notes', (req,res)=>{
-    
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 })
+router.post('/notes', (req, res) => {
+    
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        note_id: uuid(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`New Note Added!`);
+    } else {
+      res.error('Error in adding new note');
+    }
+  });
+  
+  module.exports = router;
